4a267f8603f5c97cd9a79d9b81f4e6700dfdc5d4
Paulina Salem <paulinasalem@yahoo.com>
2018-03-27 02:40:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function happyHolidaysTo(name) {
 return ("Happy holidays, you!")
 }
 function happyHolidayTo(holiday,name) {
-  return (`Happy ${holiday}, you`)
+  return (`Happy ${holiday}, you!`)
 }
